c66975413780765cec27d4994a9097236b2aef3c
evgen-kld <evgenkld@inbox.ru>
2022-01-16 16:47:40 +0100

    Refactoring code with global variables. Create pagination

--- Delivery/main.js
+++ Delivery/main.js
@@ -1,13 +1,17 @@
+let data = null;
+let param = [undefined, undefined, undefined, undefined];
+let newArray = [];
+
 function getSortedArray(array) {
-        array.sort(function (a, b) {
-            if (Number(a.rate) < Number(b.rate))
-                return -1;
-            if (Number(a.rate) > Number(b.rate))
-                return 1;
-            return 0;
-        })
-        return array
-    }
+    array.sort(function (a, b) {
+        if (Number(a.rate) < Number(b.rate))
+            return -1;
+        if (Number(a.rate) > Number(b.rate))
+            return 1;
+        return 0;
+    })
+    return array
+}
 
 function searchBtnHandler() {
     let area = document.querySelector('.area').value;
@@ -17,28 +21,92 @@ function searchBtnHandler() {
     if (area == 'null') area = undefined;
     if (district == 'null') district = undefined;
     if (type == 'null') type = undefined;
-    downloadData([area, district, type, sale]);
+    param = [area, district, type, sale];
+    createArray();
 }
 
 function pageBtnHandler(event) {
     if (event.target.dataset.page) {
         pagination(event.target.dataset.page);
-        window.scrollTo(0, 0);
+    }
+}
+
+function renderPageBtn(page) {
+    let lastPage = newArray.length / 10
+    let activeBtn = document.querySelector('.page-item.active');
+    activeBtn.classList.remove('active')
+    let firstBtn = document.querySelector('.page-item.start');
+    if (page != 1 && firstBtn.classList.contains("disabled")){
+        firstBtn.classList.remove('disabled')
+    }
+    let finishBtn = document.querySelector('.page-item.finish');
+    if (page != lastPage && finishBtn.classList.contains("disabled")){
+        finishBtn.classList.remove('disabled')
+    }
+    if (lastPage == 1) {
+        btn = document.querySelector('.pagination');
+        btn.classList.add('d-none')
+        return
+    }
+    if (page == 1 || page == 'start') {
+        let startBtn = document.querySelector('.page-item.start');
+        startBtn.classList.add('disabled')
+        let nowBtn = document.querySelector(`.page-link.b1`);
+        nowBtn.setAttribute('data-page', 1)
+        nowBtn.innerHTML = 1
+        nowBtnParent = nowBtn.closest('.page-item');
+        nowBtnParent.classList.add('active');
+        nowBtn = document.querySelector(`.page-link.b2`);
+        nowBtn.setAttribute('data-page', 2)
+        nowBtn.innerHTML = 2
+        nowBtn = document.querySelector(`.page-link.b3`);
+        nowBtn.setAttribute('data-page', 3)
+        nowBtn.innerHTML = 3
+    }
+    if (page == 2) {
+        let nowBtn = document.querySelector(`[data-page='${page}']`);
+        nowBtnParent = nowBtn.closest('.page-item');
+        nowBtnParent.classList.add('active');
+    }
+    if (page != 2 && page != 1 && page != 'finish') {
+        let nowBtn = document.querySelector('.page-link.b2');
+        nowBtn.setAttribute('data-page', page)
+        nowBtn.innerHTML = page;
+        nowBtnParent = nowBtn.closest('.page-item');
+        nowBtnParent.classList.add('active');
+        nowBtn = document.querySelector('.page-link.b1');
+        nowBtn.setAttribute('data-page', page - 1)
+        nowBtn.innerHTML = page - 1;
+        nowBtn = document.querySelector('.page-link.b3');
+        nowBtn.setAttribute('data-page', Number(page) + Number(1))
+        nowBtn.innerHTML = Number(page) + Number(1);
+    }
+    if (page == 'finish' || page == lastPage){
+        let startBtn = document.querySelector('.page-item.finish');
+        startBtn.classList.add('disabled')
+        let nowBtn = document.querySelector('.page-link.b3');
+        nowBtnParent = nowBtn.closest('.page-item');
+        nowBtnParent.classList.add('active');
+        nowBtn.setAttribute('data-page', lastPage)
+        nowBtn.innerHTML = lastPage;
+        nowBtn = document.querySelector('.page-link.b2');
+        nowBtn.setAttribute('data-page', lastPage - 1)
+        nowBtn.innerHTML = lastPage - 1;
+        nowBtn = document.querySelector('.page-link.b1');
+        nowBtn.setAttribute('data-page', lastPage - 2)
+        nowBtn.innerHTML = lastPage - 2;
     }
 }
 
 function pagination(page) {
-    let mas = []
-    for (let i = 0; i < 20; i++) {
-        mas.push((page - 1) * 10 + i)
-    }
-    for (i = 0; i < 10; i++) {
-        let elem = document.getElementById(mas[i]);
-        elem.classList.remove('d-none')
-    }
+    if (page == 'start') page = 1
+    if (page == 'finish') page == newArray.length / 10
+    let arr = newArray.slice((page - 1) * 10, (page - 1) * 10 + 10)
+    renderRecords(arr)
+    renderPageBtn(page)
 }
 
-function createBtn() {
+function createBtn(id) {
     let btnElem = document.createElement('div');
     btnElem.classList.add('col-1');
     btnElem.classList.add('d-flex');
@@ -46,7 +114,12 @@ function createBtn() {
     let textElem = document.createElement('button');
     textElem.classList.add('btn');
     textElem.classList.add('btn-success');
-    textElem.setAttribute('type', 'button');
+    textElem.classList.add('h-25');
+    textElem.classList.add('align-items-center');
+    textElem.setAttribute('data-bs-toggle', 'modal');
+    textElem.setAttribute('data-bs-target', '#task-modal');
+    textElem.setAttribute('data-action', 'create');
+    textElem.setAttribute('id', id);
     btnElem.append(textElem);
     return btnElem;
 }
@@ -99,13 +172,12 @@ function createListItemElement(place, i) {
     let element = document.createElement('div');
     element.classList.add('row');
     element.classList.add('my-3');
-    element.classList.add('d-none');
     element.id = i;
     element.append(createName(place.name));
     element.append(createType(place.typeObject));
     element.append(createAddress(place.address, place.admArea, place.district));
     element.append(createSale(place.socialPrivileges));
-    element.append(createBtn());
+    element.append(createBtn(place.id));
     return element;
 }
 
@@ -119,7 +191,7 @@ function noRecords() {
     return element;
 }
 
-function renderRecords(array){ 
+function renderRecords(array) {
     if (array.length == 0) {
         let placeList = document.querySelector('.place-list');
         placeList.innerHTML = '';
@@ -131,20 +203,19 @@ function renderRecords(array){
     for (let i = 0; i < array.length; i++) {
         placeList.append(createListItemElement(array[i], i));
     }
-    pagination(1);
 }
 
-function createArray(array, list) {
-    let newArray = []
-    for (let i = 0; i < array.length; i++) {
-        if ((array[i].admArea == list[0] || list[0] == undefined)
-                && (array[i].district == list[1] || list[1] == undefined) 
-                    && (array[i].typeObject == list[2] || list[2] == undefined)
-                         && (array[i].socialPrivileges == list[3] || list[3] == undefined)) {
-                 newArray.push(array[i])
-             }
+function createArray() {
+    newArray = [];
+    for (let i = 0; i < data.length; i++) {
+        if ((data[i].admArea == param[0] || param[0] == undefined)
+            && (data[i].district == param[1] || param[1] == undefined)
+            && (data[i].typeObject == param[2] || param[2] == undefined)
+            && (data[i].socialPrivileges == param[3] || param[3] == undefined)) {
+            newArray.push(data[i])
+        }
     }
-    renderRecords(newArray)
+    pagination(1);
 }
 
 function createSelect(array, name) {
@@ -155,37 +226,41 @@ function createSelect(array, name) {
         text.setAttribute('value', array[i])
         arrayElement.append(text);
     }
-} 
+}
 
-function createSearch(array) {
+function createSearch() {
     let area = new Set();
     let district = new Set();
     let type = new Set();
-    for (let i = 0; i < array.length; i++) {
-        area.add(array[i].admArea);
-        district.add(array[i].district)
-        type.add(array[i].typeObject)
+    for (let i = 0; i < data.length; i++) {
+        area.add(data[i].admArea);
+        district.add(data[i].district)
+        type.add(data[i].typeObject)
     }
     createSelect(Array.from(area), '.area');
     createSelect(Array.from(district), '.district');
     createSelect(Array.from(type), '.type');
 }
 
-function downloadData(list) {
+function downloadData() {
     let url = new URL('http://exam-2022-1-api.std-900.ist.mospolytech.ru/api/restaurants')
     url.searchParams.set('api_key', 'cbd284a6-b7cd-422c-b305-f3b1a413d861');
     let xhr = new XMLHttpRequest();
     xhr.open('GET', url);
     xhr.responseType = 'json';
     xhr.onload = function () {
-        createSearch(this.response);
-        createArray(getSortedArray(this.response), list);
+        data = this.response
+        data = getSortedArray(data)
+        createSearch();
+        createArray();
     }
     xhr.send();
 }
 
+
+
 window.onload = function () {
-    downloadData([undefined, undefined, undefined, undefined])
+    downloadData()
     document.querySelector('.pagination').onclick = pageBtnHandler;
     document.querySelector('.search-btn').onclick = searchBtnHandler;
 }
