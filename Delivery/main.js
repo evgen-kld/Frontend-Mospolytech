5a4e126ac7f4735c692361769ed74e7f11e75729
evgen-kld <evgenkld@inbox.ru>
2022-01-18 16:37:13 +0100

    Changed the way to create a list of places. Add button click on place

--- Delivery/main.js
+++ Delivery/main.js
@@ -2,6 +2,11 @@ let data = null;
 let param = [undefined, undefined, undefined, undefined];
 let newArray = [];
 
+function placeBtnHandler(event) {
+    let a = event.target.id
+    console.log(a)
+}
+
 function getSortedArray(array) {
     array.sort(function (a, b) {
         if (Number(a.rate) < Number(b.rate))
@@ -33,17 +38,18 @@ function pageBtnHandler(event) {
 
 function renderPageBtn(page) {
     let lastPage = newArray.length / 10
+    if (document.querySelector('.pagination.d-none')) document.querySelector('.pagination.d-none').classList.remove('d-none')
     let activeBtn = document.querySelector('.page-item.active');
     activeBtn.classList.remove('active')
     let firstBtn = document.querySelector('.page-item.start');
-    if (page != 1 && firstBtn.classList.contains("disabled")){
+    if (page != 1 && firstBtn.classList.contains("disabled")) {
         firstBtn.classList.remove('disabled')
     }
     let finishBtn = document.querySelector('.page-item.finish');
-    if (page != lastPage && finishBtn.classList.contains("disabled")){
+    if (page != lastPage && finishBtn.classList.contains("disabled")) {
         finishBtn.classList.remove('disabled')
     }
-    if (lastPage == 1) {
+    if (lastPage == 1 || lastPage == 0) {
         btn = document.querySelector('.pagination');
         btn.classList.add('d-none')
         return
@@ -81,7 +87,7 @@ function renderPageBtn(page) {
         nowBtn.setAttribute('data-page', Number(page) + Number(1))
         nowBtn.innerHTML = Number(page) + Number(1);
     }
-    if (page == 'finish' || page == lastPage){
+    if (page == 'finish' || page == lastPage) {
         let startBtn = document.querySelector('.page-item.finish');
         startBtn.classList.add('disabled')
         let nowBtn = document.querySelector('.page-link.b3');
@@ -106,78 +112,19 @@ function pagination(page) {
     renderPageBtn(page)
 }
 
-function createBtn(id) {
-    let btnElem = document.createElement('div');
-    btnElem.classList.add('col-1');
-    btnElem.classList.add('d-flex');
-    btnElem.classList.add('justify-content-center');
-    let textElem = document.createElement('button');
-    textElem.classList.add('btn');
-    textElem.classList.add('btn-success');
-    textElem.classList.add('h-25');
-    textElem.classList.add('align-items-center');
-    textElem.setAttribute('data-bs-toggle', 'modal');
-    textElem.setAttribute('data-bs-target', '#task-modal');
-    textElem.setAttribute('data-action', 'create');
-    textElem.setAttribute('id', id);
-    btnElem.append(textElem);
-    return btnElem;
-}
-
-function createSale(sale) {
-    let saleElem = document.createElement('div');
-    saleElem.classList.add('col-2');
-    saleElem.classList.add('d-flex');
-    saleElem.classList.add('justify-content-center');
-    let textElem = document.createElement('h5');
-    textElem.innerHTML = sale ? '+' : '-';
-    saleElem.append(textElem);
-    return saleElem;
-}
-
-function createAddress(address, admArea, district) {
-    let addressElem = document.createElement('div');
-    addressElem.classList.add('col-4');
-    let textElem = document.createElement('h5');
-    textElem.innerHTML = address + ' ' + admArea + ' ' + district;
-    addressElem.append(textElem);
-    return addressElem;
-}
-
-function createType(type) {
-    let typeElem = document.createElement('div');
-    typeElem.classList.add('col-2');
-    typeElem.classList.add('d-flex');
-    typeElem.classList.add('justify-content-center');
-    let textElem = document.createElement('h5');
-    textElem.innerHTML = type;
-    typeElem.append(textElem);
-    return typeElem;
-}
 
 
-function createName(name) {
-    let nameElem = document.createElement('div');
-    nameElem.classList.add('col-3');
-    nameElem.classList.add('d-flex');
-    nameElem.classList.add('justify-content-center');
-    let textElem = document.createElement('h5');
-    textElem.innerHTML = name;
-    nameElem.append(textElem);
-    return nameElem;
-}
-
-
-function createListItemElement(place, i) {
-    let element = document.createElement('div');
-    element.classList.add('row');
-    element.classList.add('my-3');
-    element.id = i;
-    element.append(createName(place.name));
-    element.append(createType(place.typeObject));
-    element.append(createAddress(place.address, place.admArea, place.district));
-    element.append(createSale(place.socialPrivileges));
-    element.append(createBtn(place.id));
+function createListItemElement(place) {
+    let element = document.querySelector('.nodeToCopy').cloneNode(true);
+    element.classList.remove('d-none');
+    element.classList.add('forDelete');
+    element.querySelector('.place-list-name').innerHTML = place.name;
+    element.querySelector('.place-list-type').innerHTML = place.typeObject;;
+    element.querySelector('.place-list-address').innerHTML = place.address;
+    element.querySelector('.place-list-area').innerHTML = place.admArea;
+    element.querySelector('.place-list-district').innerHTML = place.district;
+    element.querySelector('.place-list-sale').innerHTML = place.socialPrivileges ? '+' : '-'
+    element.querySelector('.place-list-btn').setAttribute('id', place.id);
     return element;
 }
 
@@ -185,6 +132,7 @@ function noRecords() {
     let element = document.createElement('div');
     element.classList.add('d-flex');
     element.classList.add('justify-content-center');
+    element.classList.add('noRecord');
     let textElem = document.createElement('h3');
     textElem.innerHTML = 'Записей нет!';
     element.append(textElem);
@@ -192,16 +140,15 @@ function noRecords() {
 }
 
 function renderRecords(array) {
+    let placeList = document.querySelector('.place-list');
+    if (placeList.querySelector('.noRecord')) placeList.querySelector('.noRecord').remove()
+    while (placeList.querySelector('.forDelete')) placeList.querySelector('.forDelete').remove();
     if (array.length == 0) {
-        let placeList = document.querySelector('.place-list');
-        placeList.innerHTML = '';
         placeList.append(noRecords());
         return
     }
-    let placeList = document.querySelector('.place-list');
-    placeList.innerHTML = '';
     for (let i = 0; i < array.length; i++) {
-        placeList.append(createListItemElement(array[i], i));
+        placeList.append(createListItemElement(array[i]));
     }
 }
 
@@ -263,4 +210,5 @@ window.onload = function () {
     downloadData()
     document.querySelector('.pagination').onclick = pageBtnHandler;
     document.querySelector('.search-btn').onclick = searchBtnHandler;
+    document.querySelector('.place-list').onclick = placeBtnHandler;
 }
